61b2f94da571fda6d78d681bfa99a29a4d6f5920
piyushdsalunke <piyushdsalunke@gmail.com>
2016-02-13 22:04:32 -0600

    Changed customer_add.php for bug fix and adding comments

--- customer_add.php
+++ customer_add.php
@@ -21,22 +21,23 @@ if ($conn->connect_error) die($conn->connect_error);
 	$city = $_POST['city'];
     $state = $_POST['state'];
 	$zip = $_POST['zip'];
-	$email = $_POST['add'];
-	$phone = $_POST['add'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
     
+		//insert query 
         $query = "INSERT INTO customers (firstName,lastName,address,city,state,zip,email,phone)
 					VALUES ('$first','$last','$address','$city','$state','$zip','$email','$phone')";
        // mysql_query($query); 
        if( $conn->query($query) == true){
         echo "<script type='text/javascript'>\n";
         echo "alert('Customer is succesflly added');\n"; 
-		echo "window.location.href='index.html'";
+		echo "window.location.href='index.html'"; // redirect to index.file after adding a customer successfully
         echo "</script>";
 		//header('Location: index.html') ; exit;
        }
        else
        {
-           echo "error".$query."<br>".$conn->error;
+           echo "error".$query."<br>".$conn->error; //Error message in case of problem with connection 
        }
        $conn->close(); 
 	   echo "Added Customer";
